d681f74d31dcea7ff3f7e78f3567d49b1d8b75e8
aqua0603 <80818578+g5d07ch@users.noreply.github.com>
2021-12-23 14:10:41 +0900

    Add files via upload

---
+++ 06_multimedia/opencv_image.py
@@ -0,0 +1,19 @@
+import cv2
+
+img = cv2.imread('wiwiwi.jpg')
+img2 = cv2.resize(img, (1000, 800))
+
+cv2.imshow('wiwiwi', img)
+cv2.imshow('wiwiwi2', img2)
+
+edge1 = cv2.Canny(img, 50, 100)
+edge2 = cv2.Canny(img, 100, 150)
+edge3 = cv2.Canny(img, 150, 200)
+
+cv2.imshow('edge1', edge1)
+cv2.imshow('edge2', edge2)
+cv2.imshow('edge3', edge3)
+
+cv2.waitKey(0)
+
+cv2.destroyAllwindows()
